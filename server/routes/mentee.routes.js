014a8e6584c51dbf8d99dc17781442328a513468
Brock <brock.clayton@ufl.edu>
2019-03-31 17:29:47 -0400

    added router from profile to mentee.html

--- server/routes/mentee.routes.js
+++ server/routes/mentee.routes.js
@@ -2,7 +2,7 @@ var express = require('express'),
   router = express.Router(),
   path = require('path'),
   mid = require('../middleware/mid'),
-  //User = require('../models/user');
+  User = require('../models/user');
 
 router.get('/', mid.requiresLogin, function(req,res) {
 	return res.sendFile(path.join(__dirname + '/../../client/pages/mentee.html'));
