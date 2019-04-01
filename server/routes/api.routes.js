946f28b64d885946db35126c969a58a0701a87f2
Brock <brock.clayton@ufl.edu>
2019-04-01 19:04:16 -0400

    added simple algorithm to get compatability

--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -55,10 +55,46 @@ router.get('/rating', mid.requiresLogin, function (req, res, next) {
 });
 
 router.get('/compatibility', mid.requiresLogin, function (req, res, next) {
-	data = {
-		compatibility: 100
-	};
-	return res.send(data);
+	//var user;
+	var compat = 0;
+	User.findById(req.session.userId, { password: 0 }, function (error, user) {
+		User.findOne({ username: req.query.name }, { password: 0 }, function (error, user2) {
+			
+			thirdFunction();
+			
+			function firstFunction(){
+				for (i = 0, leng = user.menteeinterests.length; i < leng; i++){
+					for (j = 0, len = user2.mentorinterests.length; j < len; j++){
+						if (user.menteeinterests[i] === user2.mentorinterests[j]){
+							compat = compat + 70;
+							break;
+						}
+					}
+				}
+				
+				//compat = compat + 70;
+				return;
+			};
+			
+			async function secondFunction(){
+				await firstFunction();
+				if (Math.abs(user.zipcode - user2.zipcode) < 200){
+					compat = compat + 30;
+				}
+				//compat = compat +30;
+				return;
+			};
+			
+			async function thirdFunction(){
+				await secondFunction();
+				var data = {
+					compatibility: compat
+				};
+				
+				res.send(data);
+			};
+		});
+	});
 });
 
 router.get('/mentees', mid.requiresLogin, function (req, res, next) {
@@ -90,3 +126,147 @@ function avg(array) {
 }
 
 module.exports = router;
+
+/*var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  hasProfile: {
+    type: Boolean,
+    required: true
+  },
+  isMentee: {
+    type: Boolean,
+    required: true
+  },
+  isMentor: {
+    type: Boolean,
+    required: true
+  },
+  googleSignIn: {
+    type: Boolean,
+    required: true
+  },
+  menteeinterests: {
+    type: [String]
+  },
+  mentorinterests: {
+    type: [String]
+  },
+  zipcode: {
+    type: Number
+  },
+  timezone: {
+    type: String
+  },
+  preferredEducation: {
+    type: String
+  },
+  ratings: {
+    type: [Number]
+  },
+  prefeducation: {
+    type: String  
+  },
+  education : {
+    type: String
+  },
+  communicationMethod: {
+    type: [String]
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  gender: {
+    type: String
+  },
+  phonenumber: {
+    type: Number
+  },
+  bio: {
+    type: String
+  },
+  friends: {
+    type: [String]
+  },
+  mentors: {
+    type: [String]
+  },
+  mentees: {
+    type: [String]
+  }
+});
+
+//authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+
+//authenticate with google
+UserSchema.statics.authenticateGoog = function (email, callback) {
+  User.findOne({ email: email })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+
+      return callback(null, user);
+
+    });
+}
+
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;*/
