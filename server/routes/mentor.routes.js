ed9ab4323e887c622202b0f0755c8a3869f1a197
Sohrab <sohrabroohi@ufl.edu>
2019-03-31 22:52:57 -0400

    Mentor survey working

--- server/routes/mentor.routes.js
+++ server/routes/mentor.routes.js
@@ -12,5 +12,31 @@ router.get('/survey', mid.requiresLogin, mid.hasProfile, function(req,res,next) 
   return res.sendFile(path.join(__dirname + '/../../client/pages/mentorSurvey.html'));
 });
 
+router.post('/survey', mid.requiresLogin, mid.hasProfile, function(req,res,next) {
+  var x = req.body.timeZone;
+  console.log(x);
+  if(req.body['method[]'] && req.body.timeZone && req.body['topics[]'] && req.body.level) {
+    var data = {
+      communicationMethod: req.body['method[]'],
+      timezone: req.body['timeZone'],
+      mentorinterests: req.body['topics[]'],
+      education: req.body['level'],
+      isMentor: true
+    }
+
+    User.updateOne({_id: req.session.userId}, data, {upsert: true}, function(error, result) {
+      if(error) {
+        console.log(err);
+        return res.redirect('/mentor/survey');
+      }
+      else {
+        return res.redirect('/mentor');
+      }
+    });
+  }
+  else {
+    return res.redirect('/mentor/survey');
+  }
+});
 
 module.exports = router;
